a8d3d4b13651b4c9514d241e5c9e3da0ec152aa3
Chizoba Emeghiebo <chizobaemeh09@gmail.com>
2025-02-05 15:43:57 +0100

    I think it works

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,53 @@
 import { useState } from "react";
-import Button from "./components/Button";
 import Container from "./components/Container";
 function App() {
   const [count, setCount] = useState(0);
+  const [winOrLose, setWinOrLose] = useState("You can do it");
 
   const colors = ["red", "light", "purple", "bluee", "green", "yellow"];
 
   const random = Math.floor(Math.random() * 6);
 
-  const handleClick = () => {
-    setCount(count + 1);
-    console.log(colors[random]);
-  };
-
   const resetGame = () => {
     setCount(0);
+    setWinOrLose("Let's gooo");
   };
 
   return (
     <>
-      <div className="bg-dark  min-h-screen flex flex-col items-center justify-center relative">
-        <h1
-          onClick={handleClick}
-          className="-mt-40 font-heading text-center text-primary text-4xl font-bold"
-        >
+      <div className="bg-dark py-8  min-h-screen flex flex-col items-center justify-center relative">
+        <h1 className="font-heading text-center text-primary text-4xl font-bold">
           Color Picker <br /> Game
         </h1>
-        <p className="my-4 w-[80%] mx-auto text-light font-lead text-center">
+        <p className="my-4 w-[80%] text-sm mx-auto text-light font-lead text-center">
           Pick the correct color that matches the rectangle to win
         </p>
-        <p className="text-light  mb-8">Score: {count}</p>
         <Container>
-          <Button bg={`bg-${colors[random]}`} />
-
+          <div
+            // onClick={() => console.log(`${colors[random]}`)}
+            className={`p-14 rounded cursor-pointer bg-${colors[random]}`}
+          ></div>
           <div className="grid grid-cols-3 gap-4 mt-4">
             {colors.map((color, id) => (
-              <Button key={id} bg={`bg-${color}`} />
+              <div
+                key={id}
+                // onClick={handleClick}
+                onClick={() => {
+                  const targetColor = colors[random];
+                  if (targetColor == color) {
+                    setCount(count + 1);
+                    setWinOrLose("Congrats! You Win!");
+                  } else {
+                    setWinOrLose("Try again! You Lose!");
+                  }
+                }}
+                className={`p-14 rounded cursor-pointer bg-${color}`}
+              ></div>
             ))}
-            {/* <Button bg="bg-red" />
-            <Button bg="bg-bluee" />
-            <Button bg="bg-light" />
-            <Button bg="bg-yellow" />
-            <Button bg="bg-green" />
-            <Button bg="bg-purple" /> */}
-            <p className={`text-${colors[random]}`}>{colors[random]}</p>
+          </div>
+          <div className="flex items-center justify-between">
+            <p className="text-light">Score: {count}</p>
+            <p className="text-light">{winOrLose}</p>
           </div>
 
           <button
